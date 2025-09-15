32a1a0798017cbb0e3de37c0e9c856cc49367a68
FulkorRain <ranygamer353@gmail.com>
2025-09-15 20:49:57 +0300

    Added lightbox

--- specific_javascript/worth_javascript.js
+++ specific_javascript/worth_javascript.js
@@ -342,3 +342,25 @@ alphaItems.forEach(group => {
 
   grid.appendChild(box);
 });
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".lightbox .close");
+
+document.querySelectorAll(".box img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+    lightboxImg.alt = img.alt;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+});
